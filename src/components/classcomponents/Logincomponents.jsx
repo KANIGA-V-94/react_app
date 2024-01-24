96b474dace53121fbe9b3d3bc8ad48c5db11303f
KANIGA-V-94 <kanigav2004@gmail.com>
2024-01-24 16:32:35 +0530

    first commit

---
+++ src/components/classcomponents/Logincomponents.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Class component Example</h1>
+            </div>
+        );
+    }
+}
+export default Login;
---
+++ src/components/functionalcomponents/testcomponents.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+//import TestClassComp from "../classcomponents/testclasscomponents";
+
+function Testcomponent() {
+    return (
+        <div>
+            <h1>This is test Functional Component</h1>
+            <p>We are learning learning functionalcomponents</p>
+        </div>
+    );
+}
+export default Testcomponent;
+//const fC = ReactDOM.createRoot(document.getElementById("root"))
+//fC.render(
+    //<Testcomponent/>
+//)
+/*
+function add(a,b){
+    return a+b
+}
+console.log(add(5,10))
+*/
